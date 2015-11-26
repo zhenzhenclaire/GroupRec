f7ed93d4138cdfc7959f1df418e5f8be3ebbd44d
zhenzhenclaire <zhenzhenclaire7@gmail.com>
2015-11-27 00:17:32 +0800

    Google API

---
+++ src/main/util/Person.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Claire on 11/26/2015.
+ */
+public class Person {
+    String latitude = "";
+    String longitude = "";
+    String userID = "";
+
+    public Person(String lat, String lon){
+        this.latitude = lat;
+        this.longitude = lon;
+    }
+
+}
