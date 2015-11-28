dd91729917c85c2674d0300415301f8554a2abfa
zhenzhenclaire <zhenzhenclaire7@gmail.com>
2015-11-28 22:58:29 +0800

    Spectral Clusterting test

--- src/main/analysis/SpectralClustering.java
+++ src/main/analysis/SpectralClustering.java
@@ -1,6 +1,43 @@
-/**
- * Created by Claire on 11/28/2015.
- */
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.mahout.clustering.spectral.kmeans.SpectralKMeansDriver;
+
+
 public class SpectralClustering {
 
+    static String input ;
+    static String output;
+    static int numDims;
+    static int clusters;
+    static int maxIterations;
+
+    public static void clustering(String[] args) throws Exception {
+        ToolRunner.run(new SpectralKMeansDriver(),args);
+    }
+
+    public static void clustering() throws Exception {
+        String[] args = new String[10];
+        args[0] = "-i";
+        args[1] = input;
+        args[2] = "-o";
+        args[3] = output;
+        args[4] = "-d";
+        args[5] = numDims + "";
+        args[6] = "-k";
+        args[7] = clusters + "";
+        args[8] = "-x";
+        args[9] = maxIterations + "";
+        clustering(args);
+    }
+
+    public static  void main(String[] args) throws Exception {
+        String dataResource = "DataResource/";
+        input = dataResource + "test.txt";
+        output = dataResource + "result";
+        numDims = 4;
+        clusters = 3;
+        maxIterations = 1;
+        clustering();
+
+
+    }
 }
